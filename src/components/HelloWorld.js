6431bf20c49ac54710c80c7e1a241fd330297304
lucaszw <lucas.zeer@gmail.com>
2019-12-01 15:07:30 -0500

    make multiline, add new characters

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,9 @@
 import * as p5 from 'p5';
 import * as _ from 'lodash';
+import delay from 'delay';
+import alphabet from './alphabetMatrix';
 
 const P5 = new p5();
-
 
 let R = 'r';
 let L = 'l';
@@ -13,58 +14,51 @@ let UR = 'ur';
 let DR = 'dr';
 let DL = 'dl';
 
-
 let screenHeight = 400;
-let screenWidth = 800;
-let boxSize = 30;
+let screenWidth = 1000;
+let boxSize = 15;
 let activeBoxes = {};
-let colorOffset = 100;
 
+window.colorOffset = 100;
 window.activeBoxes = activeBoxes;
 
-// let l_a = [D, D, D, UP, R, R, D, UP, UP, UP, L, L];
-// let l_b = [D, D, D, D, UP, UP, R, R, D, D, L, R, UP, UP, UP, UP, L, L];
-let cA = [[D,D,D,UP,UP,R,R,D,D,UP,UP,UP,UL,DL]];
-let cB = [[D,D,D,D,R,R,UP,UL,UR,UP,L,L]];
-let cC = [[D,D,D,D,DR,R,UR],[UR,R,DR]];
-let cD = [[D,D,D,D,D,D,R,R,UR,UP,UP,UP,UP,UP,UL,L,L]];
-let cE = [[D,D,D,D,D,D,R,R,R],[D,D,D,R,R],[R,R,R]];
-let cF = [[D,D,D,D,D,D],[D,D,D,R,R],[R,R,R]];
-let cG = [[D,D,D,D,DR,R,UR,UP,L],[UR,R,DR]];
-let cH = [[D,D,D,D,D,D],[D,D,D,R,R,R,UP,UP,UP],[D,D,D,R,R,R,D,D,D]];
-let cI = [[R,D,D,D,D,D,D,L],[R,D,D,D,D,D,D,R],[R,R]];
-let cJ = [[D,D,D,D,D,DL,L,UL,UP]];
-let cK = [[D,D,D,D,D,D],[D,D,D,R,UR,UR,UP],[D,D,D,R,DR,DR,D]];
-let cL = [[D,D,D,D,D,D,R,R,R]];
-let cM = [[D,D,D,D,D,D],[DR,DR,UR,UR,D,D,D,D,D,D]];
-let cN = [[D,D,D,D,D,D],[DR,DR,UR,UR],[DR,DR,R,D,D,D,D]];
-let cO = [[D,D,D,D,DR,R,UR,UP,UP,UP,UP,UL,L]];
-let cP = [[D,D,D,D,D,D],[D,D,D,R,R,UR,UP,UL,L]];
-let cQ = [[D,D,D,D,DR,UR,DR],[UR,R,DR,D,D,D]];
-let cR = [[D,D,D,D,D,D],[D,D,D,R,R,DR,D,D],[R,R,DR,D]];
-let cS = [[D,DR,R,DR,D,DL,L,UL],[UR,R,DR]];
-let cT = [[R,R,R,R],[R,R,D,D,D,D,D]];
-let cU = [[D,D,D,D,D,DR,R,UR,UP,UP,UP,UP,UP]];
-let cV = [[D,D,D,D,DR,DR,UR,UR,UP,UP,UP,UP]];
-let cW = [[D,D,D,D,D,DR,UR,UP,D,DR,UR,UP,UP,UP,UP,UP]];
-let cX = [[D,D,DR,DL,D,D,UP,UP,UR,R,DR,D,D,UP,UP,UR,UL,UP,UP]];
-let cY = [[D,D,DR,R,R,D,D,DL,DL,L,UL],[D,D,DR,R,R,UP,UP]];
-let cZ = [[R,R,R,R,D,D,DL,L,DL,D,D,R,R,R]];
+let convertStringToMatrix = (s) => {
+    let arr = _.map(_.compact(s.split("\n")),_.trim);
+    arr.width = arr[0].length;
+    arr.height = arr.length;
+    return arr;
+};
 
-let alphabet = {A: cA, B: cB, C: cC, D: cD, E: cE, F: cF, G: cG, H: cH, I: cI, J: cJ, K: cK, L: cL, M: cM, N: cN, O: cO, P: cP, Q: cQ, R: cR, S: cS, T: cT, 
-U: cU, V: cV, W: cW, X: cX, Y: cY, Z: cZ};
-console.log({alphabet});
+window.characterPosition = {index: 0, width: 0, row: 1};
 
 function drawBox(row, column) {
     let x = column * boxSize;
     let y = row * boxSize;
     let k = `${x},${y}`;
-    activeBoxes[k] = colorOffset;
-    fill(50+colorOffset,205+colorOffset,50+colorOffset);
+    activeBoxes[k] = window.colorOffset;
+    fill(50+window.colorOffset,205+window.colorOffset,50+window.colorOffset);
     square(x,y,boxSize);
     updateActiveBoxes(k);
-  }
-  
+    return k;
+}
+
+function drawMatrix(_r, _c, character) {
+    if (!character) return;
+    if (window.characterPosition.index <= 0) window.characterPosition.width = 0;
+
+    let matrix = convertStringToMatrix(character);
+    _c += window.characterPosition.width + 1;
+    window.characterPosition.width += matrix.width + 1;
+
+    matrix.forEach ((row, rowIndex) => {
+        for (let i=0;i<row.length;i++) {
+            if (row[i] == 1) drawBox(_r+rowIndex, _c+i);
+        }
+    });
+
+    window.characterPosition.index += 1;
+}
+
 function drawShape(_r, _c, shapes) {
     shapes.forEach ((shape) => {
         let r = _r;
@@ -80,12 +74,12 @@ function drawShape(_r, _c, shapes) {
             if (shape[i] == DL) {c -= 1; r +=1;}
             if (shape[i] == DR) {c += 1; r +=1;}            
             drawBox(r, c);
-        }   
+        }
     });
 }
 function depletionRate(ticks) {
     // return 6*atan(ticks/colorOffset);
-    return 3;
+    return 2;
 }
 
 function updateActiveBoxes(k) {
@@ -107,66 +101,143 @@ function getBox() {
     return [i,ii];
 }
 
-function renderActivity() {
-    let [i, ii] = getBox();
-    let x = i*boxSize;
-    let y = ii*boxSize;
+let trackItems = _.debounce((row) => {
+    if (row != window.characterPosition.row) return;
+    window.characterPosition.index -= 1;
+});
 
-    let k = `${x},${y}`;
-    activeBoxes[k] = colorOffset;
-    _.each(activeBoxes, (val, key) => {
-        let [x,y] = key.split(",");
-        if (val <= 0) {
-            fill(50,205,50);
-            noStroke();
-            square(x,y,boxSize);
-            delete activeBoxes[key];
-        } else {
-            fill(50+val,205+val,50+val);
-            square(x,y,boxSize);
-            updateActiveBoxes(key);
-        }
-    });
-
-}
 function renderGrid() {
     for (let i=0;i<screenWidth/boxSize;i++) {
         let x = i*boxSize;
         for (let ii=0;ii<screenHeight/boxSize;ii++) {
             let y = ii*boxSize;
             fill(50,205,50);
-            noStroke();
+            stroke(40,195,40);
             square(x,y,boxSize);
         }
     }
 }
 
-let s = (sk) => {    
-    
-    let keyWasPressed = false;    
-    sk.keyPressed = (e) => {
-        console.log(e.key.toUpperCase());
-        keyWasPressed = true;
+class MatrixCharacter {
+    constructor(s) {
+        this.matrix = _.map(_.compact(s.split("\n")),_.trim);
+        this.width = this.matrix[0].length;
+        this.height = this.matrix.length;
+        this.boxes = [];
+        this.ticks = 0;
+        this.maxTicks = 40;
+        this.row = null;
+        this.column = null;
     }
 
+    draw(position) {
+        if (window.characterPosition.index <= 0) {
+            window.characterPosition.width = 0;
+        }
+        this.row = window.characterPosition.row;
+        position.c += window.characterPosition.width + 1;
+        window.characterPosition.width += this.width + 1;
+
+        if (window.characterPosition.width*boxSize > screenWidth) {
+            window.characterPosition.row += 6;
+            this.row += 6;
+            position.c = 1
+            window.characterPosition.width = this.width + 1;
+            window.characterPosition.index = 0;
+        }
+        
+        this.matrix.forEach ((row, rowIndex) => {
+            for (let colIndex=0; colIndex<row.length;colIndex++) {
+                if (row[colIndex] == 1) {
+                    let r = this.row + rowIndex;
+                    this.boxes.push(drawBox(r, position.c+colIndex));
+                }
+            }
+        });
+        window.characterPosition.index += 1;
+        this.ticks = this.maxTicks;
+        this.animate();
+    }
+
+    get fade() {
+        if (this.ticks <= 0) return 0;
+        return window.colorOffset - window.colorOffset * (this.maxTicks - this.ticks)/(this.maxTicks);
+    }
+
+    async animate() {
+        if (this.ticks <= 0) {
+            trackItems(this.row); // character.position.index -= 1;
+            return;
+        }
+
+        this.ticks -= 5;
+        for (let i=0;i<this.boxes.length;i++) {
+            let box = this.boxes[i];
+            let [x,y] = box.split(",");
+            if (this.ticks <= 0) {
+                fill(50,205,50);
+                square(x,y,boxSize);
+                delete activeBoxes[box];
+            } else {
+                fill(50+this.fade,205+this.fade,50+this.fade);
+                square(x,y,boxSize);
+                updateActiveBoxes(box);
+            }
+        }
+        await delay(100);
+        this.animate();
+    }
+}
+
+
+window.writeWord = async (string) => {
+    for (let i=0;i<string.length;i++) {
+        let character = new MatrixCharacter(alphabet[string[i].toUpperCase()]);
+        character.draw({r: 1, c: 1});
+        await delay(100);
+    }
+}
+
+let s = (sk) => {    
+    
+    let keyWasPressed = false;
+    let keyThatWasPressed = null;
+    let r = 1;
+    let c = 1;
+
+    sk.keyPressed = (e) => {
+        e.preventDefault();
+        keyThatWasPressed = e.key.toUpperCase();
+        keyWasPressed = true;
+    }
+    
     sk.setup = () => {
         createCanvas(screenWidth, screenHeight);
         renderGrid();
     }
 
     sk.draw = () => {
-        renderActivity();
         if (keyWasPressed == true) {
-            drawShape(1,1,cE);
+            console.log({keyThatWasPressed});
+            if (keyThatWasPressed == "ENTER") {
+                window.characterPosition.row += 6;
+                window.characterPosition.index = 0;
+            }
+            if (keyThatWasPressed == "BACKSPACE") {
+                window.characterPosition.row -= 6;
+                window.characterPosition.index = 0;
+            }
+            let letter = alphabet[keyThatWasPressed];
+            if (letter) {
+                let character = new MatrixCharacter(letter);
+                character.draw({r, c});
+            }
         }
         keyWasPressed = false;
     }
 }
 
 async function Init() {
-    // let element = document.createElement('div');
-    // element.setAttribute('id', 'container');
-    // document.body.appendChild(element);
     const P5 = new p5(s);
 }
 
